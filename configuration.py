45f2e4cc99e8632fcd2852fe4de2b4d83db5aeab
takitakir <70449443+takitakir@users.noreply.github.com>
2025-05-09 11:48:03 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 
-api_id = 1354782
-api_hash = '24031688c3bda62e711398b2a7c2b38a'
-phone = '+639194525840'
+api_id = 11111111
+api_hash = '24031688c3bda62e711398b2a7c2b38avxv'
+phone = '+1111111111'
 client = TelegramClient(phone, api_id, api_hash)
